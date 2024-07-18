d8cff79719947bca669a3003eecdbaebeb7c3cc6
samalouty <128055267+samalouty@users.noreply.github.com>
2024-07-19 00:51:23 +0300

    Added swap functionality
    you can now swap between C to F and also F to C by clicking the ⇅ button

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-function convertTemperature() {
-  const celsiusInput = document.querySelector("#cel");
-  const fahrenheitInput = document.querySelector("#far");
+document.getElementById("convert").addEventListener("click", function () {
+  const tempInput = document.getElementById("temp-input").value;
+  const result = document.getElementById("result");
+  const conversionTitle = document.getElementById("conversion-title").innerText;
 
-  const celsius = parseFloat(celsiusInput.value);
-  const fahrenheit = parseFloat(fahrenheitInput.value);
-
-  if (!isNaN(celsius)) {
-    fahrenheitInput.value = (celsius * 9) / 5 + 32;
+  if (conversionTitle === "Celsius to Fahrenheit") {
+    result.value = (tempInput * 1.8 + 32).toFixed(2);
+  } else {
+    result.value = ((tempInput - 32) / 1.8).toFixed(2);
   }
-  // else if (!isNaN(fahrenheit)) {
-  //   celsiusInput.value = ((fahrenheit - 32) * 5) / 9;
-  // }
-  else {
-    alert("Please enter a valid number in one of the fields.");
-  }
-}
+});
 
-const convertButton = document.getElementById("convert");
-convertButton.addEventListener("click", convertTemperature);
+document.getElementById("swap").addEventListener("click", function () {
+  const conversionTitle = document.getElementById("conversion-title");
+  const tempLabel = document.getElementById("temp-label");
+  const resultLabel = document.getElementById("result-label");
+  const formula = document.getElementById("formula");
+
+  if (conversionTitle.innerText === "Celsius to Fahrenheit") {
+    conversionTitle.innerText = "Fahrenheit to Celsius";
+    tempLabel.innerHTML = 'Fahrenheit <span id="deg-f"> (°F)</span>';
+    resultLabel.innerHTML = 'Celsius <span id="deg-c"> (°C)</span>';
+    formula.innerHTML =
+      "Fahrenheit to Celsius conversion formula <br /> Celsius = (Fahrenheit - 32) / 1.8";
+  } else {
+    conversionTitle.innerText = "Celsius to Fahrenheit";
+    tempLabel.innerHTML = 'Celsius <span id="deg-c"> (°C)</span>';
+    resultLabel.innerHTML = 'Fahrenheit <span id="deg-f"> (°F)</span>';
+    formula.innerHTML =
+      "Celsius to Fahrenheit conversion formula <br /> Fahrenheit = (Celsius * 1.8) + 32";
+  }
+});
